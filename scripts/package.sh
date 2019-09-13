b5b1fd103d4e9a93d4dd983975c1ec81288dbe02
christianabraham <christian.abraham@traveloka.com>
2019-09-13 14:53:32 +0700

    - support creation of zip file based on stream and hash

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,11 +1,13 @@
 
 source scripts/config.sh
 
-ver=HEAD
+
 
 #stream="ent-coolpie"
 
 stream=$1
+
+ver=$2
 
 tmpFolder='.tmp'
 
@@ -15,8 +17,12 @@ cp -fr ent-shared $tmpFolder
 
 cp -fr shared $tmpFolder
 
-zipFile=lambda-$stream-$ver.zip
+#zipFile=lambda-$stream-$ver.zip
+zipPrefix=lambda-$stream
+zipFile=$zipPrefix.zip
 
 #cd $ver && zip -r ../build/$zipFile $APP_SOURCES && cd ..
 #cd $stream && zip -r ../.build/$zipFile $APP_SOURCES
 cd $tmpFolder && zip -r ../.build/$zipFile *  && cd ..
+
+cp ./build/$zipFile ./build/$zipPrefix-$ver.zip
